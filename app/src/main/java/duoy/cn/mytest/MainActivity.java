fc760eb6659561f6867d24a776cd2b62c9b564b4
wyx111 <2538066216@qq.com>
2016-04-09 11:56:57 +0800

    测试2

--- app/src/main/java/duoy/cn/mytest/MainActivity.java
+++ app/src/main/java/duoy/cn/mytest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //测试1提交
+        //测试2提交
     }
 }
